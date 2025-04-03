ea473c94d665b676c7df0e77c99cef1a46bfb384
Anthony <107648652+LckyClovr@users.noreply.github.com>
2025-04-03 00:25:44 -0500

    implement user retrieval in GET route with authentication middleware

--- src/routes/auth/me.ts
+++ src/routes/auth/me.ts
@@ -1,6 +1,14 @@
 import type { Request, Response } from "express";
+import authenticateRequest from "../../middlewares/authenticateRequest";
+import prisma from "../../resources/prisma";
 export const GET = [
+  authenticateRequest(),
   async (req: Request, res: Response) => {
+    const user = prisma.user.findUnique({
+      where: {
+        id: req.userId,
+      },
+    });
     return res.json({ message: "Hello World!" });
   },
 ];
